33726057ca2186af43eab04133610dadc0236ddd
João Marcelo <joaomarcelolimaoliveira7@gmail.com>
2023-04-20 17:03:52 -0300

    init js exervive

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const submitBTN = document.qurrySelector('.button-submit');
+const inputCoin = document.qurrySelector('.input-coin');
+
+submitBTN.addEventListener('click', () => {
+  console.log('dsdasdad');
+});
